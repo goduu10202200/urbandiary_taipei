e3cb939305513e5f5dbae481fec59b910c76a33e
goduu10202200 <goduu10202200@gmail.com>
2018-12-11 00:10:39 +0800

    1. 更改為mongodb

---
+++ api/DBConnection_mongo.php
@@ -0,0 +1,6 @@
+<?php
+// MongoDB 伺服器設定
+
+$dbhost = 'localhost:27017';
+$dbname = 'ud';
+date_default_timezone_set("Asia/Taipei");
--- api/taipei_ajax.php
+++ api/taipei_ajax.php
@@ -1,7 +1,8 @@
 <?php
-    include 'DBConnection.php';
+     include 'DBConnection_mongo.php';
     $raw_post_data = json_decode(file_get_contents('php://input'), true);
     
+    $kind = 'list';
     $date =  "";
     $time = "";
     $title = "";
@@ -10,47 +11,73 @@
     $id =  $raw_post_data['id'];
     $today = date("Y-m-d H:i:s");
     $date = date("Y-m-d");
-   
+    $latitude = "";
+    $longitude = "";
     switch ($id) {
         case "t1":
             $time = "22:45";
-            
             $title = "2018臺北周末音樂不斷電";
             $location = "臺北市松山文創園區文創大街";
+            $latitude = 25.044117;
+            $longitude = 121.5585953;
             break;
         case "t2":
             $time = "22:45";
             $title = "2018臺北溫泉季";
             $location = "臺北市北投區北投溫泉博物館";
+            $latitude = 25.13679;
+            $longitude = 121.5049873;
             break;
         case "t3":
             $time = "22:45";
             $title = "關渡國際自然藝術季";
             $location = "臺北市北投區關渡自然公園";
+            $latitude = 25.118957;
+            $longitude = 121.4687493;
             break;
         case "t4":
             $time = "22:45";
             $title = "2018臺北海碩盃國際女子網球挑戰賽";
             $location = "臺北市松山區臺北小巨蛋";
+            $latitude = 25.05155;
+            $longitude = 121.5475633;
             break;
         case "t5":
             $time = "22:45";
             $title = "2018士林官邸菊展";
             $location = "臺北市士林區士林官邸公園";
+            $latitude = 25.1020745;
+            $longitude = 121.533718;
             break;
         case "t6":
             $time = "22:45";
             $title = "2018臺北馬拉松";
             $location = "臺北市信義區市民廣場";
+            $latitude = 25.0371169;
+            $longitude = 121.5609001;
             break;
     }
 
-    $sql = "INSERT INTO scheduled (username, title, type, location, date, time, created_at)
-    VALUES (1, '".$title."' ,'".$type."' ,'".$location."', '".$date."', '".$time."','".$today."')";
+    // 連線資料庫
+    $manager = new MongoDB\Driver\Manager("mongodb://".$dbhost);
 
-    if ($conn->query($sql) === true) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-    $conn->close();
+    // 插入資料，BulkWrite是批量插入
+    $bulk = new MongoDB\Driver\BulkWrite;
+    // 新增資料
+    $bulk->insert([
+        'username'      => 'mau123@gmail.com',
+        'title'         => $title,
+        'content'       => '',
+        'type'          => $type,
+        'location'      => $location,
+        'latitude'      => $latitude,
+        'longitude'     => $longitude,
+        'date'          => $date,
+        'time'          => $time,
+        'status'        => 0,
+        'kind'          => $kind,
+        'created_at'    =>  $today
+    ]);
+
+    // 執行BulkWrite
+    $manager->executeBulkWrite($dbname.'.'.$collection, $bulk);
